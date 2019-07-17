11cb84140b9c5bdd83edec681f93c3a52a188602
Blake <bssmith1999@gmail.com>
2019-07-17 00:52:22 +0000

    Update Render.cpp

--- Render.cpp
+++ Render.cpp
@@ -8,11 +8,16 @@
 void render() {
   const int width = 1024;
   const int height = 768;
+  const float fov = M_PI/3.;
   std::vector<Vec3f> framebuffer(width*height);
   
+  #pragma omp parallel for
   for (size_t j =0; j<height; j++) {
     for (size_t i = 0; i<width; i++) {
-      framebuffer[i+j*width] =Vec3f(j/float(height),i/float(width),0);
+      float dir_x =(i+0.5)-width/2.;
+      float dir_y =-(j+0.5)+height/2.;
+      float dir_z = -height/(2.*tan(fov/2));
+      framebuffer[i+j*width] =cast_ray(Vec3f(dir_x, dir_y, dir_z).normalize(), spheres, lights);
     }
   }
   
